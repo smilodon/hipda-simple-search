80a2f9c1303dc331d68e0c4606e4946af9d3cc97
fqxufo <fqxufo@gmail.com>
2020-02-22 21:44:59 +0800

    add more search type

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
-let xhr = new XMLHttpRequest();
-let uInt8Array;
-let db;
-xhr.open("GET", "titles_BS.db", true);
-xhr.responseType = "arraybuffer";
+let xhr_BS = new XMLHttpRequest();
+let xhr_D = new XMLHttpRequest();
+let db_BS;
+let db_D;
 
-xhr.onload = function(e) {
-  uInt8Array = new Uint8Array(this.response);
-  db = new SQL.Database(uInt8Array);
-  let contents = db.exec("SELECT * FROM 'titles' LIMIT 0,30");
+xhr_D.open("GET", "titles_D.db", true);
+xhr_D.responseType = "arraybuffer";
+
+xhr_D.onload = function(e) {
+  let uInt8Array = new Uint8Array(this.response);
+  db_D = new SQL.Database(uInt8Array);
+  let contents = db_D.exec("SELECT * FROM 'titles' LIMIT 0,30");
 
   console.log(contents);
 };
-xhr.send();
+xhr_D.send();
+
+xhr_BS.open("GET", "titles_BS.db", true);
+xhr_BS.responseType = "arraybuffer";
+
+xhr_BS.onload = function(e) {
+  let uInt8Array = new Uint8Array(this.response);
+  db_BS = new SQL.Database(uInt8Array);
+  let contents = db_BS.exec("SELECT * FROM 'titles' LIMIT 0,30");
+
+  console.log(contents);
+};
+xhr_BS.send();
 
 // setTimeout(function() {
 //   let search = db.exec("SELECT * FROM 'titles' where title like '%iphone%'");
@@ -34,8 +48,15 @@ const app = new Vue({
     results: []
   },
   methods: {
-    search() {
+    search(type) {
+      console.log(type);
       console.log("ckick!");
+
+      let db = db_D;
+      console.log(db);
+      if (type == "bs") {
+        db = db_BS;
+      }
       this.results = db.exec(
         `SELECT * FROM 'titles' where title like '%${this.keyword}%'`
       )[0].values;
