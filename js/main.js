548f4b588fe664a61eeff7b6fc459c9ef99a8914
fqxufo <fqxufo@gmail.com>
2020-02-22 20:09:50 +0800

    edit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let uInt8Array;
 xhr.open("GET", "titles_BS.db", true);
 xhr.responseType = "arraybuffer";
 
-let.onload = function(e) {
+xhr.onload = function(e) {
   uInt8Array = new Uint8Array(this.response);
   let db = new SQL.Database(uInt8Array);
   let contents = db.exec("SELECT * FROM 'titles' LIMIT 0,30");
