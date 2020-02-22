147e72342f10a7ab376d4bd1c0a177c115a94f07
fqxufo <fqxufo@gmail.com>
2020-02-22 20:14:53 +0800

    test

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 let xhr = new XMLHttpRequest();
 let uInt8Array;
+let db;
 xhr.open("GET", "titles_BS.db", true);
 xhr.responseType = "arraybuffer";
 
 xhr.onload = function(e) {
   uInt8Array = new Uint8Array(this.response);
-  let db = new SQL.Database(uInt8Array);
+  db = new SQL.Database(uInt8Array);
   let contents = db.exec("SELECT * FROM 'titles' LIMIT 0,30");
 
   console.log(contents);
 };
 xhr.send();
+
+setTimeout(function() {
+  let search = db.exec("SELECT * FROM 'titles' where title like '%iphone%'");
+  console.log(search);
+}, 6000);
