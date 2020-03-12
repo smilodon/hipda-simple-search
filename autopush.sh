ce55377a7addbdf2d138865e9a7b59e4db19b98b
fqx <fqxufo@gmail.com>
2020-03-12 17:20:55 +0800

    update db

--- autopush.sh
+++ autopush.sh
@@ -3,6 +3,8 @@
 cd /home/ubuntu/hipda-simple-search
 python3 gettitles.py
 
+cp titles_D.db doc/
+cp titles_BS.db doc/
 git add .
 git commit -m 'update db'
 git push origin
---
+++ docs/js/main.js
@@ -0,0 +1,107 @@
+let xhr_BS = new XMLHttpRequest();
+let xhr_D = new XMLHttpRequest();
+let db_BS;
+let db_D;
+
+xhr_D.open("GET", "titles_D.db", true);
+xhr_D.responseType = "arraybuffer";
+
+xhr_D.onload = function(e) {
+  let uInt8Array = new Uint8Array(this.response);
+  db_D = new SQL.Database(uInt8Array);
+  app.db_D_loaded = true;
+  let contents = db_D.exec("SELECT * FROM 'titles' LIMIT 0,30");
+
+  console.log(contents);
+};
+xhr_D.send();
+
+xhr_BS.open("GET", "titles_BS.db", true);
+xhr_BS.responseType = "arraybuffer";
+
+xhr_BS.onload = function(e) {
+  let uInt8Array = new Uint8Array(this.response);
+  db_BS = new SQL.Database(uInt8Array);
+  app.db_BS_loaded = true;
+  let contents = db_BS.exec("SELECT * FROM 'titles' LIMIT 0,30");
+
+  console.log(contents);
+};
+xhr_BS.send();
+
+// setTimeout(function() {
+//   let search = db.exec("SELECT * FROM 'titles' where title like '%iphone%'");
+//   console.log(search);
+//   let results = search[0].values;
+
+//   for (let value of results) {
+//     console.log(value);
+//     document.write(`
+//     <h3><a href="https://www.hi-pda.com/forum//viewthread.php?tid=${value[1]}">${value[0]}</a></h3>
+//     <hr>
+//     `);
+//   }
+// }, 5000);
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    keyword: "iphone",
+    results: [],
+    db_D_loaded: false,
+    db_BS_loaded: false,
+    showd: false,
+    has_result: true
+  },
+  methods: {
+    search(type) {
+      console.log(type);
+      console.log("ckick!");
+
+      let db = db_D;
+      console.log(db);
+      if (type == "bs") {
+        db = db_BS;
+      }
+      let sqlResult = db.exec(
+        `SELECT * FROM 'titles' where title like '%${this.keyword}%' ORDER BY tid DESC`
+      );
+
+      console.log(sqlResult);
+
+      if (sqlResult.length > 0) {
+        this.has_result = true;
+        this.results = sqlResult[0].values;
+      } else {
+        this.has_result = false;
+      }
+    }
+  },
+  computed: {
+    db_D_load_status() {
+      if (this.db_D_loaded) {
+        return "D版索引已加载完成";
+      } else {
+        return "D版索引未加载,请稍后";
+      }
+    },
+    db_BS_load_status() {
+      if (this.db_BS_loaded) {
+        return "BS版索引已加载完成";
+      } else {
+        return "BS版索引未加载,请稍后";
+      }
+    }
+  },
+
+  watch: {
+    keyword() {
+      this.has_result = true;
+    }
+  }
+});
+
+// make showd true after 1 minutes
+setTimeout(() => {
+  app.showd = true;
+}, 60000);
